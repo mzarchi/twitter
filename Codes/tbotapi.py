c414c6f7448e3be2453f4ebaab66be55e931702f
Mohammad Zarchi <me@zartweety.ir>
2023-03-07 09:21:45 +0330

    Add disable web view param

--- Codes/tbotapi.py
+++ Codes/tbotapi.py
@@ -8,7 +8,13 @@ def runapi(**p):
 
     try:
         response = requests.post(
-            apiURL, json={'chat_id': p['chat_id'], 'text': p['text'], 'parse_mode': "html"})
+            apiURL, json={
+                'chat_id': p['chat_id'],
+                'text': p['text'],
+                'parse_mode': "html",
+                'disable_web_page_preview': True
+            }
+        )
         return response.text
     except Exception as e:
         return e
